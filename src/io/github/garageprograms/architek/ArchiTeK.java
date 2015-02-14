a6fe44515294b7a6b6757d72994620238ccff036
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2015-02-14 14:34:27 -0600

    Added menu bar.

--- src/io/github/garageprograms/architek/ArchiTeK.java
+++ src/io/github/garageprograms/architek/ArchiTeK.java
@@ -1,16 +1,34 @@
+/**
+ * Copyright (C) 2015 GaragePrograms
+ */
 package io.github.garageprograms.architek;
 
 import java.awt.Toolkit;
-import javax.swing.JFrame;
+import javax.swing.*;
 
+/**
+ * The main class of the program.
+ * 
+ * @author Nicolás A. Ortega
+ * @version 15.02.14
+ */
 public class ArchiTeK {
 	private JFrame frame;
+	private JMenuBar menuBar;
 
 	public ArchiTeK() {
+		// Create frame with caption
 		frame = new JFrame("ArchiTeK");
+		// Close when 'X' button is pressed
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Set size to fullscreen.
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		frame.setSize((int)tk.getScreenSize().getWidth(), (int)tk.getScreenSize().getHeight());
+
+		// Setup menu
+		menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+
 		frame.setVisible(true);
 	}
 
