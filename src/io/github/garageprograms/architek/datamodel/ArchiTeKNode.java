96b1a3691917dafef97a7d97c53f4faf15d98efb
Louis Goessling <louis@goessling.com>
2015-02-14 15:18:59 -0600

    Spacing is good

--- src/io/github/garageprograms/architek/datamodel/ArchiTeKNode.java
+++ src/io/github/garageprograms/architek/datamodel/ArchiTeKNode.java
@@ -3,7 +3,9 @@ package io.github.garageprograms.architek.datamodel;
 public class ArchiTeKNode {
 	public String name = "";
 	public String comment = "";
+
 	public void render(){};
+	
 	public boolean canApplyProperty(Property p){
 		return false;
 	}
