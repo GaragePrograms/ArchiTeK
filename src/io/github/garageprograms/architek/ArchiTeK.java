a81dc69bcc1ec0b94991d125aec51187d57ae37b
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2015-02-14 14:23:16 -0600

    Opens a window.

--- src/io/github/garageprograms/architek/ArchiTeK.java
+++ src/io/github/garageprograms/architek/ArchiTeK.java
@@ -1,8 +1,17 @@
 package io.github.garageprograms.architek;
 
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
 public class ArchiTeK {
+	private JFrame frame;
+
 	public ArchiTeK() {
-		System.out.println("Welcome to ArchiTeK!");
+		frame = new JFrame("ArchiTeK");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		frame.setSize((int)tk.getScreenSize().getWidth(), (int)tk.getScreenSize().getHeight());
+		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) { new ArchiTeK(); }
