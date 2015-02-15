71182d2f7cfc15be2cf7a46e047720f35d56fee8
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2015-02-15 02:20:21 -0600

    Fixed issue.

--- src/io/github/garageprograms/architek/DrawingPanel.java
+++ src/io/github/garageprograms/architek/DrawingPanel.java
@@ -64,10 +64,10 @@ public class DrawingPanel extends JPanel implements MouseListener, MouseMotionLi
 	public void updateFrame(){
 		this.removeAll();
 		this.addAllFilesAsJLabels();
-		frame.setResizable(false);
+		//frame.setResizable(false);
 		frame.revalidate();
 		frame.repaint();
-		frame.setResizable(true);
+		//frame.setResizable(true);
 		repaint();
 	}
 
